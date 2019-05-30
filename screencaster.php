5128492716f1a093d25f0890cfcdee8eb75d09d1
Imelda <31431447+Larusso254@users.noreply.github.com>
2019-05-30 12:33:31 +0300

    Add files via upload

---
+++ screencaster.php
@@ -0,0 +1,46 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RAT 1</title>
+</head>
+<body>
+
+	<form  method="get">
+		Required fields ***<br>
+			Value 1: <input type="number" >***<br>
+			Value 2: <input type="number" >***<br>
+			Value 3: <input type="number" ><br>
+			<br></br>
+			<input type="submit">
+	</form>
+
+<?php
+  function myRat($myvar1,$myvar2,$to_words)
+  {
+ 	$myRatResult = $myvar1+$to_words;
+  }
+ 			if(isset($_GET['submit']))  
+   				 {  
+       				$a = $_POST['myvar1'];  
+        			$b = $_POST['myvar2'];  
+        			$sum =  $a+$b;     
+
+
+     				$a = getValueA();
+     				$b = getValueB();
+
+  						if ($a,$b) {
+							if (($a,$b==!"-","")
+
+   									} else {
+											return '"  " is not a number'
+     												}
+  										$to_words=$sum
+   															}
+													echo "expected output - ".$sum; 
+														echo "expected output- $";  
+								?>			
+</body>
+</html>
